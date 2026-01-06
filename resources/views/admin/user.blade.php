bab79859bf62eda4d6d4e8c432e3c3b2da60f525
Bilawal <bilawal2407f@aptechgdn.net>
2026-01-06 13:16:00 +0500

    hello world

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -29,7 +29,6 @@
     class="flex h-screen overflow-hidden"
 >
 
-    <!-- Mobile backdrop -->
     <div
         x-show="sidebarOpen"
         x-cloak
@@ -39,7 +38,6 @@
     ></div>
 
 
-    <!-- Sidebar -->
     <aside
         class="fixed inset-y-0 left-0 z-30 w-64 bg-gray-900 text-white transform transition-transform duration-300 ease-in-out lg:translate-x-0"
         :class="sidebarOpen ? 'translate-x-0' : '-translate-x-full'"
@@ -50,8 +48,7 @@
 
 
     <div class="flex-1 flex flex-col overflow-hidden lg:ml-64 bg-gradient-to-br from-gray-50 to-gray-100">
-         <!-- Main Content -->
-            <!-- Top Bar -->
+       
             <header class="bg-white shadow-sm z-10">
                 <div class="flex justify-between items-center p-4">
                     <div>
