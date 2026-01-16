ec846a01bc78fe0de88c60107c1838b1c08e2b5e
Bilawal <bilawal2407f@aptechgdn.net>
2026-01-16 15:08:50 +0500

    all issue reolsved

--- resources/views/user/layouts/master-layouts/plain.blade.php
+++ resources/views/user/layouts/master-layouts/plain.blade.php
@@ -19,7 +19,7 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
-
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
     
@@ -43,7 +43,7 @@
             --surface-color: #FFFFFF;      
             --border-color: #E5D5C3;
         }
-
+        [x-cloak] { display: none !important; }
     </style>
 </head>
 
